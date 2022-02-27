e9d730b85c403dd301b058804c7b4b10f05c90f1
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-02-27 16:58:49 +0100

    added some css and some boilerplate code for server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 const express = require('express')
 const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hi 👋')
+
+})
+
+app.listen(5555, () => {
+    console.log('Server running on localhost:5555')
+})
