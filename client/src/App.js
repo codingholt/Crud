1377c91a3971b08565a6a9f37b8f2b0e65915b82
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-02-28 16:35:51 +0100

    made the connection with the db work and made a get request on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,36 @@ function App() {
   const [age, setAge] = useState(0)
   const [country, setCountry] = useState('')
   const [position, setPosition] = useState('')  
-  const [wage, setWage] = useState('')
+  const [wage, setWage] = useState(0)
+
+  const [employeeList, setEmployeeList] = useState([]);
 
   const addEmployee = () => {
-    Axios.post('https://localhost:3001/create', 
-    {
+    console.log(name)
+    Axios.post('http://localhost:3001/create', {
       name: name,
       age: age,
       country: country,
       position: position,
       wage: wage,
-    }).then(()=>{
+    }).then(() => {
       console.log('post request succes')
-    })
+    });
+  }
+
+  const getEmployee = () => {
+    Axios.get('http://localhost:3001/employees').then((response) => {
+      console.log(response)
+
+    });
   }
 
   return<div className="App">
   <div className='information'>
     <label>Name</label>
-    <input type='text' onChange={(event) =>{
-      setName(event.target.value)
+    <input type='text'
+    onChange={(event) =>{
+      setName(event.target.value);
     }}/>
     <label>Age</label>
     <input type='number' onChange={(event) =>{
@@ -46,7 +56,13 @@ function App() {
     onChange={(event) =>{
       setWage(event.target.value)
     }}/>
-    <button onClick={addEmployee()}>Add Employee</button>
+    <button onClick={addEmployee}>Add Employee</button>
+  </div>
+  <div className='employees'>
+  <button onClick={getEmployee}>Show Employees</button>
+  <div className='employeeData'>
+
+  </div>
   </div>
   </div>
 }
--- server/index.js
+++ server/index.js
@@ -1,35 +1,48 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = 3001
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
+
+app.use(cors())
+app.use(express.json())
 
 const db = mysql.createConnection({ //Create connection to the MySQL database
     user: 'root',
-    host: 'local',
-    password: 'Mysql0510',
+    host: 'localhost',
+    password: 'MySql0510',
     database: 'employeeSystem'
 })
 
 app.post('/create', (req, res) => {
-   const name = req.body.name;
-   const age = req.body.age;
-   const country = req.body.country;
-   const position = req.body.position;
-   const wage = req.body.wage;
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
 
-   db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)', 
-   [name, age, country, position, wage],
-    (err, result) =>{
-        if(err){
-            console.log(err)
-        } else{
-            res.send('Values Inserted')
-        }
-   }
-   );
+    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)', 
+    [name, age, country, position, wage],
+        (err, result) =>{
+            if(err){
+                console.log(err)
+            } else{
+                res.send('Values Inserted')
+            }
+    }
+    );
 })
 
+app.get('/employees', (req, res) => {
+    db.query('SELECT * FROM employees', (err, result) => {
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on https://localhost:${PORT}`)
